2367048a051f00b00f7f4c98a270d32a713ea717
WinterBlue16 <leekh090163@gmail.com>
2020-09-19 14:14:15 +0900

    변수 학습2

---
+++ 생활코딩_JAVA 1/Data_and_Operation/Variable.java
@@ -0,0 +1,15 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int a=1;// integer=정수. java에서는 변수의 타입을 지정해주지 않으면 오류가 발생한다!
+		System.out.println(a);
+		
+		double b=1.1; // double=실수.
+		System.out.println(b);
+		
+		String c="Hello World!!"; // String=문자열
+		System.out.println(c);
+	}
+
+}
