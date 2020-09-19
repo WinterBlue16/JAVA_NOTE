cdbfd70035b6bffac7c24be19cd7dca9a4b63673
WinterBlue16 <leekh090163@gmail.com>
2020-09-19 14:14:02 +0900

    변수 학습

---
+++ 생활코딩_JAVA 1/Data_and_Operation/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name="leezche";
+		System.out.println("Hello, "+name+"..."+name+"...egoing ... bye");
+		
+		double VAT=10.0; // 10.0이 아니라 10을 값으로 넣어도 출력값은 10.0으로 나온다!
+		System.out.println(VAT);
+	}
+
+}
